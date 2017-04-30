22a2147d3fae5ced5e80aa9670a0bbe55a7ca13f
FooTearth <footearth@gmail.com>
2017-04-30 23:38:21 +0800

    end keystone and osclient without compose

---
+++ 00_osclient/verify/verify.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+curl -i \
+  -H "Content-Type: application/json" \
+  -d '
+{ "auth": {
+  "identity": {
+    "methods": ["password"],
+      "password": {
+        "user": {
+          "name": "admin",
+          "domain": { "id": "default" },
+          "password": "'$OS_PASSWORD'"
+        }
+      }
+    }
+  }
+}' \
+  http://$HOST_KeyStone:5000/v3/auth/tokens ; echo
