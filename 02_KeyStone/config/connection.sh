dd4acc434706745297da1014de1f1ea7ec411b49
FooTearth <footearth@gmail.com>
2017-05-02 06:22:30 +0800

    1. adjust osclient 2. adjust keystone db port for glance 3. made glace builder script

--- 02_KeyStone/config/connection.sh
+++ 02_KeyStone/config/connection.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sed -i \
-  -e "/^#connection = */cconnection = mysql+pymysql:\/\/keystone:${OS_PASSWORD}@${HOST_DB_KeyStone}\/keystone" \
+  -e "/^#connection = */cconnection = mysql+pymysql:\/\/keystone:${OS_PASSWORD}@${HOST_DB_KeyStone}:3396\/keystone" \
   /etc/keystone/keystone.conf
 
 sed -i "/fernet_rotate\` command/{n;d}" /etc/keystone/keystone.conf
