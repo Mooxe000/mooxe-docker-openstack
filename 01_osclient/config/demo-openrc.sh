c06a43edf4f2808c3007f5c801dc45285087bb59
FooTearth <footearth@gmail.com>
2017-05-04 02:14:00 +0800

    completed keystone && glance compose

--- 01_osclient/config/demo-openrc.sh
+++ 01_osclient/config/demo-openrc.sh
@@ -1,8 +1,8 @@
 export OS_PROJECT_DOMAIN_NAME=Default
 export OS_USER_DOMAIN_NAME=Default
-export OS_PROJECT_NAME=admin
-export OS_USERNAME=admin
-export OS_PASSWORD=netserver
-export OS_AUTH_URL=http://:35357/v3
+export OS_PROJECT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=$OS_PASSWORD
+export OS_AUTH_URL=http://$HOST_KEYSTONE:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
