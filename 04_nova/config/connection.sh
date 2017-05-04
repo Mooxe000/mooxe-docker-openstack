2b0d9726bb82ca62b5f9e58ded02dbddfc2e5524
FooTearth <footearth@gmail.com>
2017-05-04 13:25:22 +0800

    adjust nova init script

--- 04_nova/config/connection.sh
+++ 04_nova/config/connection.sh
@@ -8,6 +8,6 @@ apt install -y vim
 cp /etc/nova/nova.conf /etc/nova/nova.conf.bak
 
 sed -i \
-  -e "/^connection = */cconnection = mysql+pymysql://nova:${OS_PASSWORD}@${HOST_DB_NOVAAPI}:${PORT_DB_NOVAAPI}/nova_api" \
-  -e "/^#connection=*/cconnection = mysql+pymysql://nova:${OS_PASSWORD}@${HOST_DB_NOVAAPI}:${PORT_DB_NOVAAPI}/nova_api" \
+  -e "/^connection=*/cconnection = mysql+pymysql://nova:${OS_PASSWORD}@${HOST_DB_NOVAAPI}:${PORT_DB_NOVAAPI}/nova_api" \
+  -e "/^#connection=<None>/cconnection = mysql+pymysql://nova:${OS_PASSWORD}@${HOST_DB_NOVAAPI}:${PORT_DB_NOVAAPI}/nova_api" \
   /etc/nova/nova.conf
